2d415bdbf9716a77aa081d2603ae383892701d7e
atharva-aak <atharvakalbande5@gmail.com>
2025-03-15 16:08:40 +0530

    minor changes

--- app/_components/AppSidebar.tsx
+++ app/_components/AppSidebar.tsx
@@ -70,7 +70,7 @@ export function AppSidebar() {
                 </SidebarGroup>
             </SidebarContent>
             <SidebarFooter>
-                <h2 className='p-2 text-gray-400 text-sm'>Copyright @Tubeguruji</h2>
+                <h2 className='p-2 text-gray-400 text-sm'>Group 13 CYSE</h2>
             </SidebarFooter>
         </Sidebar>
     )
