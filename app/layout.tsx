41a563b9005580f494f4c9c049c5cec9b17c9e8c
atharva-aak <atharvakalbande5@gmail.com>
2025-03-15 18:03:43 +0530

    added vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </Provider>
+        <Analytics />
       </body>
     </html>
   );
